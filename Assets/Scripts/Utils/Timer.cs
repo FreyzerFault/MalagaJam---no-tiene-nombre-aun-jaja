ff5d99687e49411e419dbcd756f15a2b876c3c48
FreyzerFault <martinezalcantara29@gmail.com>
2026-01-29 21:10:40 +0100

    TODO PREPARADOOOO lesgouuu (carpetas, paquetes basicos, readme con todos los links importantes...)

---
+++ Assets/Scripts/Utils/Timer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utils
+{
+    [Serializable]
+    public class Timer
+    {
+        public event Action OnTimerEnd;
+
+        public bool enabled = true;
+
+        public bool randomInterval;
+        
+        [ShowIf("randomInterval"), Min(0)] public float minInterval;
+        [ShowIf("randomInterval"), MinValue("@minInterval")] public float maxInterval;
+        
+        [HideIf("randomInterval")] public float interval;
+        
+        
+        public bool repeat = false;
+        protected float timeLeft;
+        
+        public bool HasEnded => timeLeft <= 0;
+
+        public Timer(float interval = 1)
+        {
+            this.interval = interval;
+            Reset();
+        }
+
+        public void Reset(bool accumulativeTime = false)
+        {
+            enabled = true;
+            timeLeft = (randomInterval ? Random.Range(minInterval, maxInterval) : interval) 
+                       + (accumulativeTime ? timeLeft : 0);
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (!enabled) return;
+            
+            timeLeft -= deltaTime;
+            
+            if (HasEnded)
+            {
+                OnTimerEnd?.Invoke();
+
+                if (repeat)
+                    Reset(true);
+                else
+                    enabled = false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,9 +10,32 @@ Hecho en Unity (6.3.2f1)
 - Paola
 - David
 
+## Paquetes de Unity usados
+
+### Tools
+
+- [Better Folders](https://assetstore.unity.com/packages/tools/gui/better-folders-311261)
+- [Better Hierarchy](https://assetstore.unity.com/packages/tools/utilities/better-hierarchy-272963)
+- [Autosave](https://assetstore.unity.com/packages/tools/utilities/autosave-43605)
+- [Screenshot Utility](https://assetstore.unity.com/packages/tools/utilities/screenshot-utility-177723)
+- [DoTween PRO](https://assetstore.unity.com/packages/tools/visual-scripting/dotween-pro-32416)
+- [PlayerPrefs Editor](https://assetstore.unity.com/packages/tools/utilities/playerprefs-editor-167903)
+- [Odin Inspector](https://odininspector.com)
+
+### Assets
+
+-
+
 ## Links importantes
 
-### Para itch.io
+**[Página de itch.io](https://freyzerfault.itch.io/malagajam-no-tiene-nombre-todava-xd)**
+**[Drive de Recursos del Equipo](https://drive.google.com/drive/folders/1ebrdOPo1HzK1WtupgQm10i9NcSqFzKJy)**
+
+### Para Unity
+
+[Mis paquetes con cosas básicas](https://github.com/FreyzerFault/Unity-Packages)
+
+### Para itch.io y Post-Producción
 
 [Guía de tamaños de imágenes para la página de ith.io](https://hedgiespresso.itch.io/itch-page-image-templates)
 
