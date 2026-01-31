ebbd4afe1da347daca477c07fe1ec3170997cecb
becky <rebeca.fernandez.w@gmail.com>
2026-01-31 05:46:29 +0100

    toma

--- Assets/Scripts/Characters/AnimalCharacter.cs
+++ Assets/Scripts/Characters/AnimalCharacter.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-public class AnimalCharacter : MonoBehaviour
+public abstract class AnimalCharacter : MonoBehaviour
 {
     public void OnPlayerNear()
     {
         //poner interfaz dialogo sin dibujo
+        //enseñardibujo1
+        //enseñardibujo2
         if (!DialogueManager.Instance.dialogueOnCourse)
         {
             DialogueManager.Instance.StartDialogue();
