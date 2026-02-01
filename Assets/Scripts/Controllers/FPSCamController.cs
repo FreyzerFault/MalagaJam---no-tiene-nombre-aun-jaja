d2196e021865cf83346540c698e97854db7ac521
David Martinez Alcantara <martinezalcantara29@gmail.com>
2026-02-01 12:16:07 +0100

    luces, colisiones y efectos de mascara,  y FOG

--- Assets/Scripts/Controllers/FPSCamController.cs
+++ Assets/Scripts/Controllers/FPSCamController.cs
@@ -15,7 +15,7 @@ namespace Controllers
         // MOUSE SENSITIVITY
         [BoxGroup("Sensitivity")] public float mouseSensitivity = 100f;
         [BoxGroup("Sensitivity")] public float camSpeedMultiplierWithMask = .5f;
-        private float CamSpeed => mouseSensitivity * (maskController.IsMaskOn ? camSpeedMultiplierWithMask : 1f);
+        private float CamSpeed => mouseSensitivity * (maskController.IsMaskOn ? camSpeedMultiplierWithMask * SanityPercent : 1f);
         
         [Space]
         
@@ -24,6 +24,13 @@ namespace Controllers
         [BoxGroup("FoV")] public float zoomMaxFoV = 70;
         [BoxGroup("FoV")] public float zoomMinFoV = 40;
         [BoxGroup("FoV")] public float zoomSmoothDuration = 0.2f;
+        
+        // CAM SHAKING
+        [BoxGroup("Shaking")] public int shakeVibrato = 2;
+        [BoxGroup("Shaking")] public float shakeMinStrength = 0;
+        [BoxGroup("Shaking")] public float shakeMaxStrength = 3;
+        [BoxGroup("Shaking")] public float[] shakeThresholds = { .8f, .5f, .2f, 0 };
+        private float ShakeStrength => Mathf.Lerp(shakeMaxStrength, shakeMinStrength, SanityPercent);
         
         private float SanityPercent => maskController.sanity / maskController.maxSanity;
         private float ZoomBySanity => Mathf.Lerp(zoomMinFoV, zoomMaxFoV, SanityPercent);
@@ -60,6 +67,16 @@ namespace Controllers
             // Actualiza el Zoom segun la Sanity pero espera a que las animaciones de Zoom al poner o quitar la mascara terminen
             if (maskController.IsMaskOn && !zoomTween.IsPlaying()) 
                 cam.fieldOfView = ZoomBySanity;
+
+
+            for (var i = 0; i < shakeThresholds.Length; i++)
+            {
+                var lowerShakeThreshold = shakeThresholds[i];
+                var upperShakeThreshold = i == 0 ? 1 : shakeThresholds[i - 1];
+                
+                if (SanityPercent <= upperShakeThreshold && SanityPercent > lowerShakeThreshold)
+                    SetCamVibration(ShakeStrength);
+            }
             
             lookInput *= CamSpeed * Time.deltaTime;
             xRotation -= lookInput.y;
@@ -81,6 +98,7 @@ namespace Controllers
         #region MASK LIMITATIONS
 
         private Tweener zoomTween;
+        private Tweener shakeTween;
         private bool IsPlaying => zoomTween != null && zoomTween.IsPlaying();
 
         private void OnMaskOn()
@@ -91,6 +109,9 @@ namespace Controllers
             
             zoomTween = cam.DOFieldOfView(ZoomBySanity, zoomSmoothDuration);
             zoomTween.Play();
+            
+            // TEMBLOR
+            shakeTween.TogglePause();
         }
 
         private void OnMaskOff()
@@ -101,8 +122,19 @@ namespace Controllers
             
             zoomTween = cam.DOFieldOfView(standardFoV, zoomSmoothDuration);
             zoomTween.Play();
+            
+            shakeTween.TogglePause();
         }
 
+        private void SetCamVibration(float strength)
+        {
+            shakeTween.Kill();
+            shakeTween = cam.DOShakePosition(99999, strength, shakeVibrato);
+            shakeTween.Play();
+            if (!maskController.IsMaskOn)
+                shakeTween.Pause();
+        }
+        
         #endregion
     }
 }
